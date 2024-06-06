93dc17d4c744c94ac5fa6093187ca2fa2d4a4602
hanny <roemhanney1@gmail.com>
2024-06-06 10:02:01 +0700

    Added my project

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 
@@ -61,68 +61,4 @@ function Footer() {
   );
 }
 
-=======
-import React from 'react';
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
-
-// Be sure to include styles at some point, probably during your bootstraping
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
-import './Menubar.css'; // Import the CSS file for Menubar
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import './Footer.css';
-
-function Footer() {
-  return (
-
-      <div className='container-fluid'>
-        <div className='footer'>
-          <div className='row'>
-            <div className='col-md-4 ft-1'>
-            <img src={'../Images/rice.png'} alt="Logo" fluid/>
-     
-              <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-              <div className='footer-icons'>
-                <ul>
-              <li><a href='#'> <i className="fa fa-facebook"></i></a></li> 
-              <li><a href='#'> <i className="fa fa-instagram"></i></a></li> 
-              <li><a href='#'> <i className="fa fa-telegram"></i></a></li> 
-              <li><a href='#'> <i className="fa fa-tiktok"></i></a></li> 
-                </ul>
-              </div>
-            </div>
-
-            <div className='col-md-4 ft-2'>
-    <h3>Our service</h3>
-<lu className='list-unstyle'>
-    <li><a href='#'>Shopping Now</a></li>
-    <li><a href='#'>New Arrival</a></li>
-    <li><a href='#'>Book collection</a></li>
-    <li><a href='#'>Best Seller</a></li>
-</lu>
-</div>
-
-<div className='col-md-4 ft-2'>
-    <h3>Our service</h3>
-    <lu className='list-unstyle'>
-        <li><a href='#'>About Us</a></li>
-        <li><a href='#'>Our Contact</a></li>
-        <li><a href='#'>Policy Private</a></li>
-        <li><a href='#'>FQA</a></li>
-    </lu>
-</div>
- </div>
- </div>
- 
-          <div className='lastfooter'>
-            <p className='text-muted'>Copyright © 2023​​ All Rights Reserved by Company Name </p>
-          </div>
-
-        </div>
-    
-
-  );
-}
-
->>>>>>> ea20c38978ad516912f6dd644651f99eb7a9662c
 export default Footer;
--- src/Components/SliderData.js
+++ src/Components/SliderData.js
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 export const SliderData = [
     {
       image: '../Images/bb.png',
@@ -15,22 +16,5 @@ export const SliderData = [
     {
       image: '../Images/cc.png',
     },
-=======
-export const SliderData = [
-    {
-      image: '../Images/bb.png',
-    },
-    {
-      image: '../Images/pp.png',
-    },
-    {
-      image: '../Images/cc.png',
-    },
-    {
-      image: '../Images/bb.png',
-    },
-    {
-      image: '../Images/cc.png',
-    },
->>>>>>> ea20c38978ad516912f6dd644651f99eb7a9662c
+
   ];
